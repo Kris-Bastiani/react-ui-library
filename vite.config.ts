6cb960f268566cdfe4f955bee95fa5d9c14e1236
Kris Bastiani <kris.bastiani@gmail.com>
2025-05-19 15:56:14 +1000

    feat(vite): configure library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,56 @@
+import {
+	dirname,
+	extname,
+	relative,
+	resolve,
+} from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import react from '@vitejs/plugin-react-swc';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	build: {
+		copyPublicDir: false,
+		lib: {
+			entry: resolve(__dirname, 'lib/main.ts'),
+			fileName: 'main',
+			formats: ['es'],
+			name: 'Main',
+		},
+		rollupOptions: {
+			external: ['react', 'react/jsx-runtime'],
+			input: Object.fromEntries(
+				glob.sync('lib/**/*.{ts,tsx}', {
+					ignore: ['lib/**/*.d.ts', 'lib/**/*.stories.tsx', '**/*.test.{ts,tsx}'],
+				}).map((file) => [
+					relative(
+						'lib',
+						file.slice(0, file.length - extname(file).length),
+					),
+					fileURLToPath(new URL(file, import.meta.url)),
+				]),
+			),
+			output: {
+				assetFileNames: 'assets/[name][extname]',
+				entryFileNames: '[name].js',
+			},
+		},
+	},
+	plugins: [
+		dts({ tsconfigPath: resolve(__dirname, 'tsconfig.lib.json') }),
+		libInjectCss(),
+		react(),
+	],
+	resolve: {
+		alias: {
+			'@lib': resolve(__dirname, 'lib'),
+			'@src': resolve(__dirname, 'src'),
+		},
+	},
 });
